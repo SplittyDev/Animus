ff3aabdeb329a2cbbd4caa8a2521cd353a8c0c73
Splitty <splittydev@gmail.com>
2015-07-31 18:45:37 +0200

    Added byte array encryption and decryption

--- src/libanimus/Cryptography/AsymmetricEncryptionProvider.cs
+++ src/libanimus/Cryptography/AsymmetricEncryptionProvider.cs
@@ -8,18 +8,36 @@ namespace libanimus
 			return PublicKeyEncryptionProvider.Encrypt (str, publicKey, true);
 		}
 
+		public static byte[] Encrypt (byte[] data, string publicKey) {
+			return PublicKeyEncryptionProvider.EncryptRaw (data, publicKey, true);
+		}
+
 		public static string Encrypt (string str, KeyPair keys) {
 			return Encrypt (str, keys.PublicKey);
+		}
+
+		public static byte[] Encrypt (byte[] data, KeyPair keys) {
+			return Encrypt (data, keys.PublicKey);
 		}
 
 		public static string Decrypt (string str, string privateKey) {
 			return PublicKeyEncryptionProvider.Decrypt (str, privateKey, true);
 		}
 
+		public static byte[] Decrypt (byte[] data, string privateKey) {
+			return PublicKeyEncryptionProvider.DecryptRaw (data, privateKey, true);
+		}
+
 		public static string Decrypt (string str, KeyPair keys) {
 			if (!keys.HasPrivateKey)
 				throw new Exception ("The KeyPair doesn't contain a private key.");
 			return Decrypt (str, keys.PrivateKey);
+		}
+
+		public static byte[] Decrypt (byte[] data, KeyPair keys) {
+			if (!keys.HasPrivateKey)
+				throw new Exception ("This KeyPair doesn't contain a private key.");
+			return Decrypt (data, keys.PrivateKey);
 		}
 	}
 }
