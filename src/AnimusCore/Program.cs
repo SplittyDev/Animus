acb96fd115cb6e91e1261b114d21398377445f1c
Splitty <splittydev@gmail.com>
2015-08-03 22:48:51 +0200

    Added Animus Core

---
+++ src/AnimusCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using libanimus;
+
+namespace AnimusCore
+{
+	class MainClass
+	{
+		IrcClient client;
+
+		public static void Main (string[] args) {
+			new MainClass ().Main ();
+		}
+
+		public MainClass () {
+			client = new IrcClient ();
+		}
+
+		public void Main () {
+			client.Connect ("int0x10.com", 6697);
+			client.Join ("#OperationMythicDawn");
+			while (true) {
+			}
+		}
+	}
+}
