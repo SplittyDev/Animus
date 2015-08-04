89e869e553333b42fb10a0eb8134128b6bb44f7e
Splitty <splittydev@gmail.com>
2015-08-04 02:24:25 +0200

    Did some more work on the irc client

--- src/libanimus/Network/irc/ConnectionFailedException.cs
+++ src/libanimus/Network/irc/ConnectionFailedException.cs
@@ -1,23 +1,24 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace libanimus {
 	[Serializable]
 	public class ConnectionFailedException : Exception {
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:ConnectionFailedException"/> class
+		/// Initializes a new instance of the <see cref="ConnectionFailedException"/> class
 		/// </summary>
 		public ConnectionFailedException () {
 		}
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:ConnectionFailedException"/> class
+		/// Initializes a new instance of the <see cref="ConnectionFailedException"/> class
 		/// </summary>
 		/// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
 		public ConnectionFailedException (string message) : base (message) {
 		}
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:ConnectionFailedException"/> class
+		/// Initializes a new instance of the <see cref="ConnectionFailedException"/> class
 		/// </summary>
 		/// <param name="message">A <see cref="T:System.String"/> that describes the exception. </param>
 		/// <param name="inner">The exception that is the cause of the current exception. </param>
@@ -25,11 +26,11 @@ namespace libanimus {
 		}
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:ConnectionFailedException"/> class
+		/// Initializes a new instance of the <see cref="ConnectionFailedException"/> class
 		/// </summary>
 		/// <param name="context">The contextual information about the source or destination.</param>
 		/// <param name="info">The object that holds the serialized object data.</param>
-		protected ConnectionFailedException (System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base (info, context) {
+		protected ConnectionFailedException (SerializationInfo info, StreamingContext context) : base (info, context) {
 		}
 	}
 }
--- src/libanimus/Network/irc/IrcClient.cs
+++ src/libanimus/Network/irc/IrcClient.cs
@@ -1,43 +1,203 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Security;
 using System.Net.Sockets;
-using System.IO;
 using System.Security.Authentication;
-using System.Threading;
 using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace libanimus {
 	public class IrcClient {
+
+		#region Public Properties
+
+		/// <summary>
+		/// Gets the server.
+		/// </summary>
+		/// <value>The server.</value>
 		public string Server { get; private set; }
+
+		/// <summary>
+		/// Gets the port.
+		/// </summary>
+		/// <value>The port.</value>
 		public int Port { get; private set; }
 
+		/// <summary>
+		/// Gets a value indicating whether this instance is connected.
+		/// </summary>
+		/// <value><c>true</c> if this instance is connected; otherwise, <c>false</c>.</value>
 		public bool IsConnected { get; private set; }
-		public bool IsJoined { get; private set; }
 
+		/// <summary>
+		/// Gets a value indicating whether this instance has joined.
+		/// </summary>
+		/// <value><c>true</c> if this instance has joined; otherwise, <c>false</c>.</value>
+		public bool HasJoined { get; private set; }
+
+		/// <summary>
+		/// Gets the stream.
+		/// </summary>
+		/// <value>The stream.</value>
 		public Stream Stream { get; private set; }
+
+		/// <summary>
+		/// Gets the reader.
+		/// </summary>
+		/// <value>The reader.</value>
 		public StreamReader Reader { get; private set; }
+
+		/// <summary>
+		/// Gets the writer.
+		/// </summary>
+		/// <value>The writer.</value>
 		public StreamWriter Writer { get; private set; }
 
+		/// <summary>
+		/// Gets the nickname.
+		/// </summary>
+		/// <value>The nickname.</value>
 		public string Nickname { get; private set; }
 
+		#endregion
+
+		#region Private Fields
+
+		/// <summary>
+		/// The actions.
+		/// </summary>
+		readonly ICollection<HostAction> actions;
+
+		/// <summary>
+		/// The GUID.
+		/// </summary>
+		readonly Guid guid;
+
+		/// <summary>
+		/// The identifier.
+		/// </summary>
+		readonly string id;
+
+		/// <summary>
+		/// The validation callback.
+		/// </summary>
+		RemoteCertificateValidationCallback validationCallback;
+
+		#endregion
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="libanimus.IrcClient"/> class.
+		/// </summary>
 		public IrcClient () {
-			this.IsConnected = false;
-			this.IsJoined = false;
+			actions = new List<HostAction> ();
+			guid = Guid.NewGuid ();
+			id = string.Format ("animus{0}", new string (guid.ToString ("N").Take (16).ToArray ()));
+			IsConnected = false;
+			HasJoined = false;
 		}
 
-		public void Connect (string server, int port, IrcClientConnectionOptions options) {
-			this.Server = server;
-			this.Port = port;
-			TcpClient client = new TcpClient (this.Server, this.Port);
+		/// <summary>
+		/// Connect to specified server:port using the specified options.
+		/// </summary>
+		/// <param name="server">Server.</param>
+		/// <param name="port">Port.</param>
+		/// <param name="ssl">Whether the connection should use SSL..</param>
+		public void Connect (string server, int port, bool ssl, RemoteCertificateValidationCallback callback = null) {
+			validationCallback = callback;
+			if (validationCallback == null)
+				validationCallback = new RemoteCertificateValidationCallback ((sender, certificate, chain, sslPolicyErrors) => true);
+			_Connect (server, port, ssl);
+			while (!IsConnected) {}
+			USER (id);
+			NICK (id);
+			while (!HasJoined) {}
+			MODE (Nickname, "+B");
+		}
+
+		/// <summary>
+		/// Registers an action.
+		/// </summary>
+		/// <param name="action">Action.</param>
+		public void RegisterAction (HostAction action) {
+			actions.Add (action);
+		}
+
+		/// <summary>
+		/// Sends a raw command.
+		/// </summary>
+		/// <param name="format">Format.</param>
+		/// <param name="args">Arguments.</param>
+		public void SendRaw (string format, params object[] args) {
+			var sb = new StringBuilder ();
+			sb.AppendFormat (format, args);
+			sb.Append ("\r\n");
+			Writer.Write (sb);
+			Writer.Flush ();
+		}
+
+		#region Raw IRC commands
+
+		public void USER (string username, string realname = null) {
+			if (string.IsNullOrEmpty (realname))
+				realname = username;
+			SendRaw ("USER {0} 0 * :{1}", username, realname);
+		}
+
+		public void NICK (string nickname) {
+			SendRaw ("NICK {0}", nickname);
+			Nickname = nickname;
+		}
+
+		public void MODE (string nickname, string modes) {
+			SendRaw ("MODE {0} {1}", nickname, modes);
+		}
+
+		public void JOIN (string channel) {
+			SendRaw ("JOIN {0}", channel);
+		}
+
+		#endregion
+
+		#region User-friendly wrappers
+
+		public void LogIn (string username, string realname, string nickname) {
+			USER (username, realname);
+			NICK (nickname);
+		}
+
+		public void Mode (string mode) {
+			MODE (Nickname, mode);
+		}
+
+		public void Join (string channel) {
+			JOIN (channel);
+		}
+
+		#endregion
+
+		#region Private methods
+
+		/// <summary>
+		/// Connect to specified server:port using the specified options.
+		/// </summary>
+		/// <param name="server">Server.</param>
+		/// <param name="port">Port.</param>
+		/// <param name="ssl">Whether the connection should use SSL.</param>
+		void _Connect (string server, int port, bool ssl) {
+			
+			Server = server;
+			Port = port;
+			var client = new TcpClient (Server, Port);
 
 			if (client.Connected) {
 				Stream = client.GetStream ();
 
-				if (options.Ssl) {
-					var sslCallback = options.SslCertificateValidationCallback;
-					Stream = new SslStream (client.GetStream (), false, sslCallback);
+				if (ssl) {
+					Stream = new SslStream (client.GetStream (), false, validationCallback);
 					try {
-						((SslStream)Stream).AuthenticateAsClient (this.Server);
+						((SslStream)Stream).AuthenticateAsClient (Server);
 					} catch (AuthenticationException e) {
 						Console.WriteLine ("Exception: {0}", e.Message);
 						if (e.InnerException != null) {
@@ -49,65 +209,42 @@ namespace libanimus {
 					}
 				}
 			} else {
-				var exceptionText = string.Format ("Could not connect to {0}:{1}", this.Server, this.Port);
+				var exceptionText = string.Format ("Could not connect to {0}:{1}", Server, Port);
 				throw new ConnectionFailedException (exceptionText);
 			}
 
-			this.IsConnected = true;
+			IsConnected = true;
 
-			this.Reader = new StreamReader (this.Stream);
-			this.Writer = new StreamWriter (this.Stream);
+			Reader = new StreamReader (Stream);
+			Writer = new StreamWriter (Stream);
 
-			Thread thread = new Thread (new ThreadStart(delegate() {
+			Task.Factory.StartNew (() => {
+				string line;
 				while (true) {
-					string line = "";
-					while ((line = this.Reader.ReadLine()) != null) {
+					while ((line = Reader.ReadLine ()) != null) {
 						Console.WriteLine (line);
 
-						string[] commandParts = new string[line.Split(' ').Length];
-						commandParts = line.Split(' ');
-						if (commandParts[0].Substring(0, 1) == ":") {
-							commandParts[0] = commandParts[0].Remove(0, 1);
+						var commandParts = line.Split (' ');
+						if (commandParts [0].Substring (0, 1) == ":") {
+							commandParts [0] = commandParts [0].Remove (0, 1);
 						}
 
-						if (commandParts[0].Contains(this.Server))
-							switch (commandParts[1]) {
+						if (commandParts [0].Contains (Server))
+							switch (commandParts [1]) {
 						case "001":
-							this.IsJoined = true;
+							HasJoined = true;
 							break;
 							}
 
 						if (commandParts[0] == "PING") {
-							this.Write("PONG {0}", commandParts[1]);
+							SendRaw ("PONG {0}", commandParts [1]);
 						}
 					}
 				}
-			}));
-			thread.Start ();
+			});
 		}
 
-		public void LogIn (string username, string realname, string nickname) {
-			this.Write ("USER {0} 0 * :{1}", username, realname);
-			this.Write ("NICK {0}", nickname);
-
-			this.Nickname = nickname;
-		}
-
-		public void Join (string channel) {
-			this.Write ("JOIN {0}", channel);
-		}
-
-		public void Mode (string modes) {
-			this.Write ("MODE {0} {1}", this.Nickname, modes);
-		}
-
-		public void Write(string format, params object[] args) {
-			StringBuilder sb = new StringBuilder ();
-			sb.AppendFormat (format, args);
-			sb.Append ("\r\n");
-			this.Writer.Write (sb.ToString ());
-			this.Writer.Flush ();
-		}
+		#endregion
 	}
 }
 
