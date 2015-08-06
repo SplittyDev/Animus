7717873944010f82c4ff0ab53450a6e502b71520
Splitty <splittydev@gmail.com>
2015-08-06 21:18:51 +0200

    Updated Animus Core

--- src/AnimusCore/Program.cs
+++ src/AnimusCore/Program.cs
@@ -1,33 +1,79 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using libanimus;
+using libanimus.Actions;
+using libanimus.Actions.Predefined;
+using libanimus.Networking;
+using System.Threading.Tasks;
 
 namespace AnimusCore
 {
 	class MainClass
 	{
-		readonly IrcClient client;
+		NetworkManager	network;
+		ActionPool		actions;
+		IrcUpstream		ircUpstream;
+		public string	IRC_HOST;
+		public int		IRC_PORT;
 
 		public static void Main (string[] args) {
 			new MainClass ().Main ();
 		}
 
 		public MainClass () {
-			client = new IrcClient ();
+
+			// Catch unhandled exceptions in tasks
+			TaskScheduler.UnobservedTaskException += (sender, args) => {
+				Console.WriteLine (args.Exception.Message);
+				args.SetObserved();
+			};
+
+			// Update check
+			Console.WriteLine ("Performing startup checks...");
+			Updater.Instance.StartupCheck ();
+
+			// Basic config
+			IRC_HOST = "int0x10.com";
+			IRC_PORT = 6697;
+			
+			// Get instances
+			network = NetworkManager.Instance;
+			actions = ActionPool.Instance;
+
+			// IRC upstream
+			Console.WriteLine ("Connecting to IRC...");
+			ircUpstream = new IrcUpstream ();
+			ircUpstream.Connected += (sender, e) => ircUpstream.Join ("#OperationMythicDawn");
+			ircUpstream.Connect (IRC_HOST, IRC_PORT);
+			network.RegisterUpstream (ircUpstream);
+
+			// Telnet upstream
+			Console.WriteLine ("Connecting to Telnet...");
+			network.RegisterUpstream (new TelnetUpstream ());
+
+			// Actions
+			Console.WriteLine ("Registering actions...");
+			actions.Register<ActionCd> ();
+			actions.Register<ActionPwd> ();
+			actions.Register<ActionUpdate> ();
+			actions.Register<ActionUpgrade> ();
+			actions.Register<ActionAutoUpgrade> ();
+			actions.Register<ActionKill> ();
+			actions.Register<ActionStartArgs> ();
+			actions.Register<ActionDetect> ();
+			actions.Register<ActionScreen> ();
+			actions.Register<ActionLs> ();
+			actions.Register<ActionWhoami> ();
+			actions.Register<ActionBroadcast> ();
 		}
 
 		public void Main () {
-			client.Connect ("int0x10.com", 6697, true);
-			client.Join ("#OperationMythicDawn");
-			client.OnChannelMessage += (message, sender) =>
-				Console.WriteLine ("[{0}] {1}", sender, message);
-			client.OnPrivateMessage += (message, sender) =>
-				Console.WriteLine ("[{0}] {1}", sender, message);
 			Idle ();
 		}
 
 		static void Idle () {
-			Thread.Sleep (Timeout.InfiniteTimeSpan);
+			Thread.Sleep (-1);
 		}
 	}
 }
---
+++ src/AnimusCore/SimpleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AnimusCore
+{
+	public class SimpleConfig
+	{
+		readonly string[] lines;
+
+		public SimpleConfig (string path) {
+			lines = File.ReadAllLines (path);
+		}
+
+		public string GetField (string field) {
+			return new string (
+				lines.Single (str => str.StartsWith (field))
+				.SkipWhile (c => c != ':')
+				.Skip (1)
+				.ToArray ());
+		}
+	}
+}
+
